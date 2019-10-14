63368080b85173ba3b6729e5bcb13b997deb9178
dare2python <31766642+Dare2python@users.noreply.github.com>
2019-10-14 18:06:30 +0300

    working routing

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const m = require("mithril");
 
-m.render(document.body, m("button", "hi"));
+const Step1 = require("./view/step1");
+const Step2 = require("./view/step2");
+
+m.route(document.body, "step1", {
+    "/step1": Step1,
+    "/step2": Step2,
+});
---
+++ view/step1.js
@@ -0,0 +1,7 @@
+const m = require("mithril");
+
+module.exports = {
+    view: (vnode) =>
+        m("h1", "step1"
+        )
+};
