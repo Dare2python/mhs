be6bffd3838278f7251fe31ec34339969962a4e9
dare2python <31766642+Dare2python@users.noreply.github.com>
2019-10-15 16:07:50 +0300

    crypto.generateKeyPair in node 12.12?

--- model/genkey.js
+++ model/genkey.js
@@ -1,32 +1,27 @@
+const crypto = require('crypto');
+
 const GenKey = {
     publicKey: "",
     privateKey: "",
     keyPair: null,
     generateKey: () =>
-        window.crypto.subtle.generateKey(
-            {
-                name: "RSA-OAEP",
-                // Consider using a 4096-bit key for systems that require long-term security
-                modulusLength: 2048,
-                publicExponent: new Uint8Array([1, 0, 1]),
-                hash: "SHA-256",
+        crypto.generateKeyPair('rsa', {
+            modulusLength: 4096,
+            publicKeyEncoding: {
+                type: 'spki',
+                format: 'pem'
             },
-            true,
-            ["encrypt", "decrypt", "wrapKey", "unwrapKey"]
-        ).then( (keyPair) => {
-            GenKey.keyPair = keyPair;
-            window.crypto.subtle.wrapKey(
-                "raw",
-                keyPair.publicKey,
-                keyPair.privateKey,
-                "AES-KW"
-            ).then( (res)=>{
-                console.log(res);
-            });
+            privateKeyEncoding: {
+                type: 'pkcs8',
+                format: 'pem',
+                cipher: 'aes-256-cbc',
+                passphrase: 'top secret'
+            }
+        }, (err, publicKey, privateKey) => {
+            GenKey.publicKey = publicKey;
+            GenKey.privateKey = privateKey;
             console.log(GenKey);
-
         })
-
 };
 
 module.exports = GenKey;
