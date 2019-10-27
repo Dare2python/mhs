dd5ab8bb7fa414a68c9c01ce8cae212474a8a8d5
dare2python <31766642+Dare2python@users.noreply.github.com>
2019-10-27 13:22:23 +0200

    play with net.createServer

--- src/model/tcpserver.js
+++ src/model/tcpserver.js
@@ -1,9 +1,24 @@
 const net = require('net');
 
+const PORT = 3443;
+
 const server = net.createServer(function(socket) {
-    socket.write('Echo server\r\n');
+    console.log(`${socket.localAddress}:${socket.localPort}->${socket.remoteAddress}:${socket.remotePort}`);
+    socket.write(`${socket.remoteAddress}:${socket.remotePort}\r\n`);
+
+    socket.on('end', () => {
+        console.log('client disconnected');
+    });
+
     socket.pipe(socket);
 });
 
-server.listen(3443, '127.0.0.1');
+server.on('error', (err) => {
+    console.log(err);
+});
+
+server.listen(PORT, '0.0.0.0',() => {
+    console.log(`server bound to ${PORT}`);
+});
 // netcat 127.0.0.1 3443
+// lsof -i -n -P
