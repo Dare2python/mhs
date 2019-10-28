b31b8e5d76ede7022fbfde21442ba27ecd3780ef
dare2python <31766642+Dare2python@users.noreply.github.com>
2019-10-28 12:11:17 +0200

    more play with TCP server

--- src/model/tcpserver.js
+++ src/model/tcpserver.js
@@ -6,19 +6,22 @@ const server = net.createServer(function(socket) {
     console.log(`${socket.localAddress}:${socket.localPort}->${socket.remoteAddress}:${socket.remotePort}`);
     socket.write(`${socket.remoteAddress}:${socket.remotePort}\r\n`);
 
+    console.log("%j", socket);
+
     socket.on('end', () => {
         console.log('client disconnected');
     });
 
     socket.on('data', function(data){
         console.log(data);
+        console.log("%j", data);
         const textChunk = data.toString('utf8');
         console.log(textChunk);
     });
 });
 
 server.on('error', (err) => {
-    console.log(err);
+    console.error(new Error().stack);
 });
 
 server.listen(PORT, '0.0.0.0',() => {
@@ -27,3 +30,4 @@ server.listen(PORT, '0.0.0.0',() => {
 // netcat -c -v -x 127.0.0.1 3443
 // lsof -i -n -P
 // telnet 127.0.0.1 3443
+// brew cask install packetsender
