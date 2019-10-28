0f22ab623ee553ad91cb83c9a7ed337aab0be325
dare2python <31766642+Dare2python@users.noreply.github.com>
2019-10-28 13:11:58 +0200

    grid-template-areas CSS

--- src/view/step1.js
+++ src/view/step1.js
@@ -3,13 +3,13 @@ const genKey = require("../model/genkey");
 
 module.exports = {
     view: (vnode) =>
-        m(".step1",[
-            m("button.button", {
+        m("#step1",[
+            m("button.button#head", {
                 autofocus: true,
                 type: "button",
                 onclick: () => genKey.generateKey()
-            }, "gen key"),
-            m("textarea[placeholder=Public Key]", {rows:6, cols:65, value:genKey.publicKey}),
-            m("textarea[placeholder=Private Key]", {rows:15, cols:65, value:genKey.privateKey})
+            }, "generate key"),
+            m("textarea[placeholder=Public Key]#public", {rows:6, cols:65, value:genKey.publicKey}),
+            m("textarea[placeholder=Private Key]#private", {rows:15, cols:65, value:genKey.privateKey})
         ])
 };
